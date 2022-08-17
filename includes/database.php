294b32632151856b7e806622584342755b6e1032
chax255 <ronald.chaula@live.com>
2022-08-17 18:01:24 +0300

    renamed the connection object to oversee the whole connection process online.

--- includes/database.php
+++ includes/database.php
@@ -16,9 +16,12 @@ $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$connection = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 if(mysqli_connect_error()){
 echo mysqli_connect_error();
 exit;
+}
+else{
+    echo "connection successful";
 }

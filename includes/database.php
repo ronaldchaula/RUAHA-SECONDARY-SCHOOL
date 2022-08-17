e61c65383d8ee2c496ab913561b2dd9431444e35
chax255 <ronald.chaula@live.com>
2022-08-17 12:53:17 +0300

    changed local enviroment variable to remote ones hosted on heroku

--- includes/database.php
+++ includes/database.php
@@ -1,11 +1,22 @@
 <?php
 
-$server = 'localhost';
-$database = "ruahaschool";
-$username = "admin";
-$password = "sK0AS5BEbGLBmvH0";
+//$server = 'localhost';
+//$database = "ruahaschool";
+//$username = "admin";
+//$password = "sK0AS5BEbGLBmvH0";
+//
+//$connection = mysqli_connect($server, $username, $password,  $database);
 
-$connection = mysqli_connect($server, $username, $password,  $database);
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 if(mysqli_connect_error()){
 echo mysqli_connect_error();
